d63a201074561716588eed16ff5ff471f41b795a
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-07-09 17:53:01 -0300

    feat: Adiciona suporte ao tamanho do componente Letter e ajusta estilo para LetterUsed

--- src/components/LettersUsed/index.tsx
+++ src/components/LettersUsed/index.tsx
@@ -1,11 +1,16 @@
 import styles from './styles.module.css';
 
-type Props = {};
+// importando o componente de letter(letras)
+import { Letter } from '../Letter';
 
 export function LetterUsed() {
   return (
     <div className={styles.lettersUsed}>
       <h5>Letras utilizadas</h5>
+      <div>
+        <Letter value="s" size="small" />
+        <Letter value="s" size="small" />
+      </div>
     </div>
   );
 }
