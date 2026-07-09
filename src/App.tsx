a1256e707af499e6334adb06c1384cef002e609d
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-07-09 12:10:07 -0300

    feat: Adiciona estrutura de header ao App e aprimora estilos do componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import { Header } from './components/Header';
 export default function App() {
   return (
     <div className={styles.container}>
-      <Header />
+      <main>
+        <Header />
+      </main>
     </div>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,21 @@
-import styles from './styles.module.css';
 import logo from '../../assets/logo.png';
+import restart from '../../assets/restart.svg';
+
+import styles from './styles.module.css';
 
 export function Header() {
   return (
     <div className={styles.container}>
       <img src={logo} alt="Logo" />
+
+      <header>
+        <span>
+          <strong>5</strong> de 10 tentativas
+        </span>
+        <button type="button">
+          <img src={restart} alt="Icone de reiniciar" />
+        </button>
+      </header>
     </div>
   );
 }
