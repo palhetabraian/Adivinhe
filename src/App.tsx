7a89ad299ae64c5ca69554d7d8736c225c1f2131
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-07-09 15:47:05 -0300

    feat: Adiciona componente Input e integra ao App com estilo correspondente

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import styles from './app.module.css';
 import { Header } from './components/Header';
 import { Tip } from './components/Tip';
 import { Letter } from './components/Letter';
+import { Input } from './components/Input';
 
 export default function App() {
   function handleRestartGame() {
@@ -20,6 +21,12 @@ export default function App() {
           <Letter value="a" />
           <Letter value="c" />
           <Letter value="t" />
+        </div>
+
+        <h4>Palpite</h4>
+
+        <div>
+          <Input autoFocus maxLength={1} placeholder="?" />
         </div>
       </main>
     </div>
